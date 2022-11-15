2fe1c9a078ed6889e23d6f7cdcc86da266377e80
Gianni963 <gianni_guatieri@hotmail.com>
2022-11-15 19:37:01 +0100

    first commit

---
+++ app/Http/Controllers/Backend/CommunityController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use App\Http\Requests\CommunityStoreRequest;
+use App\Models\Community;
+
+class CommunityController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $communities = Community::paginate(5)->through(fn($community) => [
+            'id' => $community->id,
+            'name' => $community->name,
+            'slug' => $community->slug
+        ]);
+        
+        return Inertia::render('Communities/Index', compact('communities'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return Inertia::render('Communities/Create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CommunityStoreRequest $request)
+    {
+        Community::create($request->validated() + ['user_id' => auth()->id()]);
+        
+        return to_route('communities.index')->with('message', 'La communauté a bien été créée');;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Community $community)
+    {
+        return Inertia::render('Communities/Edit', compact('community'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(CommunityStoreRequest $request, Community $community)
+    {
+        $community->update($request->validated());
+
+        return to_route('communities.index')->with('message', 'La communauté a bien été modifiée');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Community $community)
+    {
+        $community->delete();
+        return back()->with('message', 'La communauté a bien éte supprimée.');
+    }
+}
---
+++ app/Models/Community.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+
+class Community extends Model
+{
+    use HasFactory,Sluggable ;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'description',
+        'slug'
+    ];
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
+}
